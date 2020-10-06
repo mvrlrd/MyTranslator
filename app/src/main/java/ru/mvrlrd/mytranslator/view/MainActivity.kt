87c735b23edd5ccbb2659121c81887f6e5f21c20
mvrlrd <igrbelykh@gmail.com>
2020-10-06 19:46:37 +0300

    Initial commit

---
+++ app/src/main/java/ru/mvrlrd/mytranslator/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.mvrlrd.mytranslator.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.mvrlrd.mytranslator.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

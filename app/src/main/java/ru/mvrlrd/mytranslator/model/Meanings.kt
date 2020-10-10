4ea6df9f027cb6922affeda686cf172b4eface73
mvrlrd <igrbelykh@gmail.com>
2020-10-10 20:45:24 +0300

    got translation. removed some functions from ApiHelper and Interceptor class

--- app/src/main/java/ru/mvrlrd/mytranslator/model/Meanings.kt
+++ app/src/main/java/ru/mvrlrd/mytranslator/model/Meanings.kt
@@ -1,8 +1,9 @@
 package ru.mvrlrd.mytranslator.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 class Meanings(
-    @field:SerializedName("translation") val translation: Translation?,
+    @Expose@field:SerializedName("translation") val translation: Translation?,
     @field:SerializedName("imageUrl") val imageUrl: String?
 )
